9f4efd17037f2a808dc6a36af4246529720ce12c
jtryann <100149196+jtryann@users.noreply.github.com>
2022-04-15 15:18:52 -0400

    Add files via upload

---
+++ weatherconnector.py
@@ -0,0 +1,9 @@
+# Weather connector
+# finding weather based on race location
+# NWS (US National Weather Service) data API will be used
+# ZIP Code to lat/long API used to convert ZIP codes from the racereg API to the weather API
+
+def convertZIP(zipcode):
+    
+
+https://api.weather.gov/points/{latitude},{longitude}
