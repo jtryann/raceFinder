4fc1e04d9e8ff89484849fe2e90efcff712f9f6f
jtryann <100149196+jtryann@users.noreply.github.com>
2022-04-15 16:34:32 -0400

    Add files via upload

--- runregconnector.py
+++ runregconnector.py
@@ -8,8 +8,6 @@ import json, os, requests, pprint
 # Initializing all possible search terms for this particular API
 name, region, states, distance, eventtype, year, startDate, endDate, startpage = '', '','','','','','','',''
 
-url = "" # url is a global because we may want to use the build_url function or send_request function in one of our other files without using both
-
 pretty_parameter_labels = ['Race Name', 'Region', 'State', 'Race Distance', 'Event Type', 'Year', 'Search Start Date', 'Search End Date', 'Results Page #']
 parameter_labels = ['name', 'region', 'state', 'distance', 'eventtype', 'year', 'startDate', 'endDate', "startpage"]
 search_parameters = [name, region, states, distance, eventtype, year, startDate, endDate, startpage]
@@ -21,22 +19,22 @@ search_parameters = [name, region, states, distance, eventtype, year, startDate,
 #             1 == 1
 
 def build_url(parameters):
-    x = 0
+    x = 0 # counter to go through the list
     search_for = ""
     for i in parameters:    # If a parameter is empty, it doesn't search for it. If the parameter contains a value, then it will be added to the search
         if i != "":
             search_for += parameter_labels[x] + "=" + parameters[x] + "&"
             x += 1
     base_url = "http://www.RunReg.com/api/search/"
-    global url 
     url = base_url + "?" + search_for
+    return url
 
-def send_request():
+def send_request(url):
     site_response = requests.get(url)
     site_response.raise_for_status() # checks for errors in the API, whether it is properly functioning or not
     raceJson = json.loads(site_response.text)
-    print(raceJson)
+    return raceJson
 
 # Main program
-build_url(search_parameters)
-send_request()
+link = build_url(search_parameters)
+send_request(link)
--- weatherconnector.py
+++ weatherconnector.py
@@ -1,9 +1,42 @@
 # Weather connector
 # finding weather based on race location
-# NWS (US National Weather Service) data API will be used
-# ZIP Code to lat/long API used to convert ZIP codes from the racereg API to the weather API
+# NWS (US National Weather Service) API will be used for weather data
+# ZIP Code Geocode API to lat/long API used to convert ZIP codes from the racereg API to the weather API
 
-def convertZIP(zipcode):
-    
+import json, requests, pprint
 
-https://api.weather.gov/points/{latitude},{longitude}
+
+# functions!
+def convertZIP(zipcode): # converts ZIP code into Lat/Long
+    location_url = 'https://geocode.xyz/' + str(zipcode) + '}?json=1&region=US&auth=436040529742063285307x126692'
+    response_zip = requests.get(location_url)
+    response_zip.raise_for_status()
+    location = json.loads(response_zip.text)
+    latitude = location["latt"]
+    longitude = location["longt"]
+    return latitude, longitude
+
+ # This function part searches for the 2.5km grid that the NWS splits the US into, and that the ZIP code is located in
+def findGrid(latitude, longitude): 
+    grid_url = 'https://api.weather.gov/points/' + str(latitude) + ',' + str(longitude)
+    response_grid = requests.get(grid_url)
+    response_grid.raise_for_status()
+    grid_location = json.loads(response_grid.text)
+    nws_office = grid_location['properties']['cwa'] # selects NWS office for locale
+    x_grid = grid_location['properties']['gridX'] # selects grid X coordinate
+    y_grid = grid_location['properties']['gridY'] # selects grid Y coordinate
+    return nws_office, x_grid, y_grid
+
+# Finally, now we can get the weather!
+def getWeather(office, gridX, gridY):
+    weather_url = 'https://api.weather.gov/gridpoints/'+ office + '/' + str(gridX) + ','+ str(gridY) + '/forecast'
+    response_weather = requests.get(weather_url)
+    response_weather.raise_for_status()
+    weatherdata = json.loads(response_weather.text)
+    return weatherdata
+
+
+# Main program
+lat, long = convertZIP(31522)
+office, gridX, gridY = findGrid(lat, long)
+pprint.pprint(getWeather(office, gridX, gridY))
