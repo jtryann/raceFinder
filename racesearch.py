47dc630ccd0ee6d16552ce7b1186d4a22015daa9
jtryann <100149196+jtryann@users.noreply.github.com>
2022-04-21 17:18:27 -0400

    Add files via upload

--- racesearch.py
+++ racesearch.py
@@ -1,6 +1,6 @@
 # This file collects user inputs, displays race and weather info, and displays in tkinter
 
-import runregconnector as race, weatherconnector as weather, pprint, requests, time, tkinter
+import runregconnector as race, weatherconnector as weather, pprint
 
 name, region, states, distance, eventtype, year, startDate, endDate, startpage = '', '','','','','','','',''
 
@@ -8,15 +8,19 @@ pretty_parameter_labels = ['Race Name', 'Region', 'State', 'Race Distance', 'Eve
 parameter_labels = ['name', 'region', 'state', 'distance', 'eventtype', 'year', 'startDate', 'endDate', "startpage"]
 search_parameters = [name, region, states, distance, eventtype, year, startDate, endDate, startpage]
 
-
-raceData = race.send_request(race.build_url(search_parameters))
-matchingEvents = raceData['MatchingEvents']
-eventsData = {}
-
-for i in matchingEvents:
-    myDict = {}
-    myDict['event_name'] = i.get('')
-
+def sortRaceData():
+    raceData = race.send_request(race.build_url(search_parameters))
+    matchingEvents = raceData['MatchingEvents']
+    eventsData = {}
+    for i in matchingEvents:
+        eventsData['event_name'] = i.get('EventName')
+        eventsData['ZIP'] = i.get('EventZip')
+        eventsData['city'] = i.get('EventCity')
+        eventsData['state'] = i.get('EventState')
+        eventsData['types'] = i.get('EventTypes')
+        eventsData['categories'] = i.get('Categories')
+        eventsData['URL'] = i.get('EventUrl')
+    return eventsData
 
 # Write file
 def write_file(data):
@@ -24,5 +28,3 @@ def write_file(data):
     file = open(file_name, "w")
     file.write(str(data))
     file.close()
-
-print(len(matchingEvents))
